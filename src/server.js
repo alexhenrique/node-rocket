7c03dc759fb173c06835af176e095ae0065c48ec
Alexandro Henrique Loch <alexsctj@gmail.com>
2023-05-07 19:12:50 -0300

    JSON

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
 import http from 'node:http';
-
+const users = []
 const server = http.createServer((req, res) =>{
   const {method, url } = req
-
   if (method === 'GET' && url ==='/users'){
-    return res.end('Listagem de usuarios')
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url ==='/users'){
+    users.push({
+      id:1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
     return res.end('Criação de usuários')
   }
-
   return res.end("ola mundoooo")
 })
 
