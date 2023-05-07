2abcbe580f354e8e8d701484036a0b3aa511c20f
Alexandro Henrique Loch <alexsctj@gmail.com>
2023-05-07 18:28:21 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import http from 'node:http';
+
+const server = http.createServer((req, res) =>{
+  const {method, url } = req
+
+  if (method === 'GET' && url ==='/users'){
+    return res.end('Listagem de usuarios')
+  }
+
+  if (method === 'POST' && url ==='/users'){
+    return res.end('Criação de usuários')
+  }
+
+  return res.end("ola mundoooo")
+})
+
+server.listen(3333)
+
